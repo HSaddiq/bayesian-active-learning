0896c607e8c9b9e7b96d758927a70a1f07234899
HSaddiq <hsaddiq99@gmail.com>
2023-01-22 07:20:13 +0000

    Finalised experiments

--- src/bayesian_active_learning/models.py
+++ src/bayesian_active_learning/models.py
@@ -12,13 +12,9 @@ class BayesianConvNet(hk.Module):
     def __init__(
         self,
         num_classes: int,
-        activation: Callable[[jnp.ndarray], jnp.ndarray],
-        # dropout_rates: Tuple[float, float],
     ):
         super().__init__()
         self.num_classes = num_classes
-        self.activation = activation
-        # self.dropout_rates = dropout_rates
 
     def __call__(
         self,
@@ -28,16 +24,16 @@ class BayesianConvNet(hk.Module):
         out = input
 
         out = hk.Conv2D(output_channels=32, kernel_shape=4)(out)
-        out = self.activation(out)
+        out = nn.relu(out)
         out = hk.Conv2D(output_channels=32, kernel_shape=4)(out)
-        out = self.activation(out)
+        out = nn.relu(out)
         out = hk.MaxPool(window_shape=2, strides=1, padding="VALID")(out)
         if dropout_rates[0] != 0:
             out = hk.dropout(hk.next_rng_key(), dropout_rates[0], out)
         flat = hk.Flatten()
         out = flat(out)
         out = hk.Linear(output_size=128)(out)
-        out = self.activation(out)
+        out = nn.relu(out)
         if dropout_rates[1] != 0:
             out = hk.dropout(hk.next_rng_key(), dropout_rates[1], out)
         logits = hk.Linear(output_size=self.num_classes)(out)
@@ -45,13 +41,24 @@ class BayesianConvNet(hk.Module):
         return logits
 
 
-def define_model(
+# def define_model(
+#     num_classes: int,
+#     dropout_rates: Tuple[float, float],
+#     # activation: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu,
+# ) -> Callable[[jnp.ndarray], jnp.ndarray]:
+#     net = BayesianConvNet(
+#         num_classes=num_classes,
+#         activation=activation,
+#     )
+#     return partial(net, dropout_rates)
+
+
+def model(
     num_classes: int,
     dropout_rates: Tuple[float, float],
-    activation: Callable[[jnp.ndarray], jnp.ndarray] = nn.relu,
-) -> Callable[[jnp.ndarray], jnp.ndarray]:
+    x: jnp.ndarray,
+) -> jnp.ndarray:
     net = BayesianConvNet(
         num_classes=num_classes,
-        activation=activation,
     )
-    return partial(net, dropout_rates)
+    return net(dropout_rates, x)
